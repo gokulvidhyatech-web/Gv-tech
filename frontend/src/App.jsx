5d10765f87701a97dc518d3ddd4b7c5c99f75cd4
gokulvidhyatech-web <gokulvidhyatech@gmail.com>
2026-07-24 11:45:09 +0530

    tesssssssss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from "./pages/Login/Enterance";
-import Dashboard from "./pages/Dashboard/Authenticate";
+import Login from "./way/Login/Enterance";
+import Dashboard from "./way/Dashboard/Authenticate";
 
 function App() {
   return (
